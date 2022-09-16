1a4d2531e8c5dcaed9f56507c5960501d406e8aa
hehonglu123 <hehonglu123@126.com>
2022-09-16 12:15:28 -0400

    tormach updates

--- test_script/traj_ros.py
+++ test_script/traj_ros.py
@@ -7,8 +7,6 @@ from robot_jog_msgs.msg import *
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
-import matplotlib.pyplot as plt
-
 
 joint_positions=[]
 joint_positions_history=[]
@@ -35,10 +33,9 @@ def main():
 	JE.execute=True
 
 
-	# joint_names=['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6',]
-	joint_names=['joint_6']
+	joint_names=['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6',]
+
 	#jogging to initial position
-	# joint_positions[-1]=0.
 	#wait the service to be advertised, otherwise the service use will fail
 	rospy.wait_for_service('/jog/absolute/set_joints')
 	 
@@ -53,7 +50,7 @@ def main():
 	#jog to 0
 	while np.linalg.norm(joint_positions[-1])>0.001:
 		pub.publish(JE)
-		rtval=srv(joint_names,[0.])
+		rtval=srv(joint_names,[0.]*6)
 		rate.sleep()
 
 
@@ -67,7 +64,7 @@ def main():
 		Tj.header.stamp = rospy.Time()
 		Tjp = JointTrajectoryPoint()
 		Tjp.positions = [np.sin(t)/4.,np.sin(t)/4.,np.sin(t)/4.,np.sin(t)/4.,np.sin(t)/4.,np.sin(t)/4.]
-		vel = (Tjp.positions[5] - last_pos) / 1000.
+		# vel = (Tjp.positions[5] - last_pos) / 1000.
 
 		Tjp.time_from_start = rospy.Duration()
 		Tjp.time_from_start.secs=int(t)
